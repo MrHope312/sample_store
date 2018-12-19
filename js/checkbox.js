a2db38c93479e383f3acda6c75ea36f60e2b5310
MrHope312 <shrey@hope.com>
2018-12-20 03:47:34 +0530

    Final Commits

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,60 +1,59 @@
-function func()
-        {
-            var chb = document.getElementsByClassName('chb');
-            var card = document.getElementsByClassName('card');
-          
-            for(var i=0;i<=8;i++){
-                card[i].style.display='none';}
-                 
-            if(chb[0].checked)
-            {
-              for(var i=0;i<=2;i++){
-                card[i].style.display='inline';}
-                
-            }
-            if(!chb[0].checked)
-            {
-                for(var i=0;i<=2;i++){
-                card[i].style.display='none';}
-            }
-            
-            if(chb[1].checked)
-            {
-                for(var i=3;i<=5;i++){
-                card[i].style.display='inline';}
-            }
-            if(!chb[1].checked)
-            {
-                for(var i=3;i<=5;i++){
-                card[i].style.display='none';}
-            }
-            
-            if(chb[2].checked)
-            {
-                for(var i=6;i<=8;i++){
-                card[i].style.display='inline';}
-            }
-            if(!chb[2].checked)
-            {
-                for(var i=6;i<=8;i++){
-                card[i].style.display='none';}
-            }
-            if(chb[3].checked)
-            {
-                for(var i=9;i<=11;i++){
-                card[i].style.display='inline';}
-            }
-            if(!chb[3].checked)
-            {
-                for(var i=9;i<=11;i++){
-                card[i].style.display='none';}
-            }
+function func() {
+    var chb = document.getElementsByClassName('chb');
+    var card = document.getElementsByClassName('card');
 
-            if(!chb[0].checked && !chb[1].checked && !chb[2].checked &&  !chb[3].checked ){
-                for(var i=0;i<=11;i++){
-                card[i].style.display='inline';}
-            }
-            
+    for (var i = 0; i <= 8; i++) {
+        card[i].style.display = 'none';
+    }
+
+    if (chb[0].checked) {
+        for (var i = 0; i <= 2; i++) {
+            card[i].style.display = 'inline';
         }
 
-     
+    }
+    if (!chb[0].checked) {
+        for (var i = 0; i <= 2; i++) {
+            card[i].style.display = 'none';
+        }
+    }
+
+    if (chb[1].checked) {
+        for (var i = 3; i <= 5; i++) {
+            card[i].style.display = 'inline';
+        }
+    }
+    if (!chb[1].checked) {
+        for (var i = 3; i <= 5; i++) {
+            card[i].style.display = 'none';
+        }
+    }
+
+    if (chb[2].checked) {
+        for (var i = 6; i <= 8; i++) {
+            card[i].style.display = 'inline';
+        }
+    }
+    if (!chb[2].checked) {
+        for (var i = 6; i <= 8; i++) {
+            card[i].style.display = 'none';
+        }
+    }
+    if (chb[3].checked) {
+        for (var i = 9; i <= 11; i++) {
+            card[i].style.display = 'inline';
+        }
+    }
+    if (!chb[3].checked) {
+        for (var i = 9; i <= 11; i++) {
+            card[i].style.display = 'none';
+        }
+    }
+
+    if (!chb[0].checked && !chb[1].checked && !chb[2].checked && !chb[3].checked) {
+        for (var i = 0; i <= 11; i++) {
+            card[i].style.display = 'inline';
+        }
+    }
+
+}
